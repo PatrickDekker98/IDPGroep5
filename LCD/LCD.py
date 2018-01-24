67c3c1a4706e5f868f75be4d0ea2c84153cd69e7
Nickie <nickie.tiernagan@student.hu.nl>
2018-01-25 00:40:08 +0100

    added more code

--- LCD/LCD.py
+++ LCD/LCD.py
@@ -19,38 +19,50 @@ import RPi.GPIO as GPIO
 import time
 
 # Define GPIO to LCS mapping
-LCD_RS  = 26
-LCD_E   = 19
-LCD_D4  = 13
-LCD_D5  = 6
-LCD_D6  = 5
-LCD_D7  = 11
-GPIOoutLST = [LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7]
+class LCD:
+    #PIN Setup
+    LCD_RS = 26
+    LCD_E = 19
+    LCD_D4 = 13
+    LCD_D5 = 6
+    LCD_D6 = 5
+    LCD_D7 = 11
 
-#Define device constants
-LCD_WIDTH   = 16
-LCD_CHR     = True
-LCD_CMD     = False
+    GPIO-outLST = [LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7]
 
-LCD_LINE_1  = 0x80
-LCD_LINE_2  = 0xC0
+    #Define device constants
+    LCD_WIDTH   = 16
+    LCD_CHR     = True      #RS Character
+    LCD_CMD     = False     #RS Command
 
-# Timing constants
-E_PULSE = 0.00005
-E_DELAY = 0.00005
+    LCD_LINE_1  = 0x80  #Ram Locations
+    LCD_LINE_2  = 0xC0  #Ram Locations
 
-def __init__(self):
-    GPIO.setmode(GPIO.BCM)          # Use BCM GPIO numbers
-    GPIO.setup(GPIOoutLST, GPIO.OUT)
-    lcd_byte(0x33, LCD_CMD)  # 110011 Initialise
-    lcd_byte(0x32, LCD_CMD)  # 110010 Initialise
-    lcd_byte(0x06, LCD_CMD)  # 000110 Cursor move direction
-    lcd_byte(0x0C, LCD_CMD)  # 001100 Display On,Cursor Off, Blink Off
-    lcd_byte(0x28, LCD_CMD)  # 101000 Data length, number of lines, font size
-    lcd_byte(0x01, LCD_CMD)  # 000001 Clear display
-    time.sleep(E_DELAY)
+    # Timing constants
+    E_PULSE = 0.0005
+    E_DELAY = 0.0005
 
-def lcd_byte(bits, mode):
+    def __init__(self):
+        time.sleep(0.020)
+        GPIO.setmode(GPIO.BCM)   # Use BCM GPIO numbers
+        GPIO.setup(GPIOoutLST, GPIO.OUT)
+        lcd_byte(0x33, LCD_CMD)  # 110011 Initialise
+        lcd_byte(0x32, LCD_CMD)  # 110010 Initialise
+        lcd_byte(0x06, LCD_CMD)  # 000110 Cursor move direction
+        lcd_byte(0x0C, LCD_CMD)  # 001100 Display On,Cursor Off, Blink Off
+        lcd_byte(0x28, LCD_CMD)  # 101000 Data length, number of lines, font size
+        lcd_byte(0x01, LCD_CMD)  # 000001 Clear display
+        time.sleep(E_DELAY)
+
+    def lcd_byte(bits, mode):
 
 
-    #https://www.raspberrypi-spy.co.uk/2012/07/16x2-lcd-module-control-using-python/
+except KeyboardInterrupt:
+    GPIO.cleanup()
+finally:
+    GPIO.cleanup()
+
+#https://www.raspberrypi-spy.co.uk/2012/07/16x2-lcd-module-control-using-python/
+#https://www.openhacks.com/uploadsproductos/eone-1602a1.pdf
+#http://oomlout.com/parts/LCDD-01-datasheet.pdf
+#https://www.tutorialspoint.com/python/bitwise_operators_example.htm
